abbb50a000a9efb91c7bebe2533ad7bd0b07564f
Matheus <mat.alvessantana@gmail.com>
2024-02-05 15:52:57 -0300

    feat: add unlink after send response

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,59 @@
-import express, { Request, Response } from 'express';
-import multer from 'multer';
-import { Layer, readPsd } from 'ag-psd';
-import * as fs from 'fs';
-import cors from 'cors';
-import 'ag-psd/initialize-canvas';
-import { stringify } from 'flatted';
+import express, { Request, Response } from "express";
+import multer from "multer";
+import { Layer, readPsd } from "ag-psd";
+import * as fs from "fs";
+import cors from "cors";
+import "ag-psd/initialize-canvas";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Seja bem vindo ao PSD Reader!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Seja bem vindo ao PSD Reader!");
 });
 
-app.post('/upload', upload.single('psdFile'), async (req: Request, res: Response) => {
-  const psdFile = req.file;
+app.post(
+  "/upload",
+  upload.single("psdFile"),
+  async (req: Request, res: Response) => {
+    const psdFile = req.file;
 
-  if (!psdFile) {
-    return res.status(400).json({ error: 'No file uploaded.' });
+    if (!psdFile) {
+      return res.status(400).json({ error: "No file uploaded." });
+    }
+
+    try {
+      const psdBuffer = fs.readFileSync(psdFile.path);
+      const psd = readPsd(psdBuffer);
+
+      const layers = psd.children?.map((layer: Layer, index) => {
+        const canvas = layer.children;
+        console.log("psdcanvas", canvas);
+        const imagesUrl = canvas?.map(({ canvas }) => {
+          return canvas?.toDataURL();
+        });
+        return { id: index, name: layer.name, imagesUrl };
+      });
+
+      res.status(200).send({ layers });
+    } catch (error) {
+      console.error(`Something went wrong: ${error}`);
+      res
+        .status(400)
+        .json({
+          error:
+            "Internal server error reading the PSD file, please try again.",
+        });
+    } finally {
+      fs.unlinkSync(psdFile.path);
+      console.log(`${psdFile.path} was removed.`);
+    }
   }
-  console.log('psd recieved', psdFile)
-  // Parse the PSD file using the ag-psd library
-  const psdBuffer = fs.readFileSync(psdFile.path);
-  const psd = readPsd(psdBuffer);
-  console.log("psd parsed", psd)
-  // Convert each layer to an image and return them in a JSON format
-  const layers = psd.children?.map((layer: Layer, index) => {
-    const canvas = layer.children
-    console.log('psdcanvas', canvas)
-    const imagesUrl = canvas?.map(({canvas}) => {
-      return canvas?.toDataURL()
-    })
-    return { id: index, name: layer.name, imagesUrl };
-  });
-
-  res.status(200).send({ layers });
-});
+);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
